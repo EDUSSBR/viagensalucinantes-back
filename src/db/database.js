6ac853cbaaa787aceecb4b0b03b227e49de2e316
Eduardo Souto dos Santos <bls.dudu@gmail.com>
2023-05-26 17:11:44 -0300

    chore: setup database connection

---
+++ src/db/database.js
@@ -0,0 +1,18 @@
+import pg from 'pg';
+import dotenv from 'dotenv';
+dotenv.config();
+const { Pool } = pg;
+let connection;
+const configDatabase = {
+    connectionString: process.env.DATABASE_URL,
+};
+// if (process.env.MODE === "prod") configDatabase.ssl = true;
+
+try{
+    connection = new Pool(configDatabase);
+    await connection.connect().then(()=>console.log("Postgres connected!"));
+}catch(e) {
+    console.log(e);
+}
+
+export default connection;
